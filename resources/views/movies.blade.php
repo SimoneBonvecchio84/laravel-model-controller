80d40b7336fe52029ce1d2db7f0f4ccbaba7a72e
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-10 14:39:34 +0200

    Feat: Add MovieController and route

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app');
+
+@section('content')
+<h2>Films</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use App\Http\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/movies',[MovieController::class, 'index'])->name('books');
